8e3766008f95626c966db4797d4803882765526c
returX <543851436@qq.com>
2020-03-22 12:26:29 +0800

    5A级景区识别 后台代码

--- src/main/java/com/example/demo03/controller/uploadController.java
+++ src/main/java/com/example/demo03/controller/uploadController.java
@@ -11,8 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
